cd57515222c644aff803e914c77e375348333c1d
Natalie <natalie.huang99@gmail.com>
2020-07-29 21:03:32 -0700

    folders!

--- client/src/components/FaceDetails/UserProfile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UserProfile(props){
-    return(
-        <h3 className="user-profile">Logged in as {props.userInfo[0]}</h3>
-    )
-}
-
-export default UserProfile
--- client/src/components/SongFolder/PlaylistLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function PlaylistLink(props){
-  
-    return <a href={props.link} target="_blank" rel="noopener noreferrer">{props.value}</a>
-}
-
-export default PlaylistLink
