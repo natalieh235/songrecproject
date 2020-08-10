64ae5c2f52069a146eca57a75dd02243412ed38c
Natalie <natalie.huang99@gmail.com>
2020-08-10 12:11:52 -0700

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div className="home-page">
         <h1>Upload your face. We pick the songs.</h1>
         <img alt='' src={Face} style={{display: "inline-block"}}></img> <br />
-        <a href="http://localhost:1000">
+        <a href="https://bsr-client.herokuapp.com/">
           <button className="green-btn">Login with Spotify</button>
         </a>   
     </div>
