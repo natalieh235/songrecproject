7e59beeae2761f4cb123bb25762e5af1d3cf5d23
Natalie <natalie.huang99@gmail.com>
2020-08-11 11:59:14 -0700

    Update Webcam.js

--- client/src/components/FormFolder/Webcam.js
+++ client/src/components/FormFolder/Webcam.js
@@ -12,39 +12,40 @@ class Cam extends React.Component {
     }
 
     render() {
-        return (
-          <div id='image-submission'>
-            
-            <div id="webcam-div">
+      return (
+        <div id='image-submission'>
+          
+          <div id="webcam-div">
             <h1>Take a picture:</h1>
-              <Webcam
-                audio={false}
-                ref={node => this.webcam = node}
-                screenshotFormat='image/jpeg'
-                style={{width: '100%'}}
-              />
-              <button 
-                className="green-btn" 
-                onClick={(event) => this.props.handleCapture(this.webcam.getScreenshot())}
-                style={{width: "50%" }}
-                >Capture</button>
-
-                
-            </div>
-
-            <div id='screenshots'>
-              <h1>Image</h1>
-              {this.props.img ? <img src={this.props.img} /> : <img src={notfound} style={{width: '100%'}}/>}
-              <button 
-                className="green-btn" 
-                onClick={(event) => this.props.submit(event)}
-                style={{display: this.props.showButton ? 'inline-block' : 'none', width: "50%"}}
-                >Submit</button>
-            </div>
+            <Webcam
+              audio={false}
+              ref={node => this.webcam = node}
+              screenshotFormat='image/jpeg'
+              mirrored={true}
+              style={{width: '100%'}}
+            />
+            <button 
+              className="green-btn" 
+              onClick={(event) => this.props.handleCapture(this.webcam.getScreenshot())}
+              style={{width: "50%" }}
+              >Capture
+            </button>
           </div>
 
-        );
-      }
+          <div id='screenshots'>
+            <h1>Image</h1>
+            {this.props.img ? <img src={this.props.img} /> : <img src={notfound} style={{width: '100%'}}/>}
+            <button 
+              className="green-btn" 
+              onClick={(event) => this.props.submit(event)}
+              style={{display: this.props.showButton ? 'inline-block' : 'none', width: "50%"}}
+              >Submit
+            </button>
+          </div>
+        </div>
+
+      );
+    }
 }
 
 export default Cam
