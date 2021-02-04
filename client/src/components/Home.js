fa5ce2780e87fecfa1d19b03ac3da0b4ff78ad42
Natalie <natalie.huang99@gmail.com>
2021-02-04 11:11:27 -0800

    HREF

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Face from './faces.png'
-//"https://bsr-client.herokuapp.com/"
+
 function Home() {
-  // let handleClick = () => {
-  //   fetch(`${process.env.REACT_APP_BACKEND_URL}/login`)
-  // }
   console.log('hi')
   console.log(process.env.REACT_APP_BACKEND_URL)
   return (
@@ -12,7 +9,7 @@ function Home() {
         <h1>Upload your face. We pick the songs.</h1>
         <img alt='' src={Face} style={{display: "inline-block"}}></img> 
         <br />
-        <a href={`${process.env.REACT_APP_BACKEND_URL}/login`}>
+        <a href={process.env.REACT_APP_BACKEND_URL}>
           <button className="green-btn">Login with Spotify</button>
         </a>
     </div>
