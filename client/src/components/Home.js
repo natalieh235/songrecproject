903371c7dd29131741004105b21d09c3aaf4c5ce
Natalie <natalie.huang99@gmail.com>
2021-02-04 10:59:29 -0800

    merge

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Face from './faces.png'
 //"https://bsr-client.herokuapp.com/"
 function Home() {
+  // let handleClick = () => {
+  //   fetch(`${process.env.REACT_APP_BACKEND_URL}/login`)
+  // }
+  console.log('hi')
+  console.log(process.env.REACT_APP_BACKEND_URL)
   return (
     <div className="home-page">
         <h1>Upload your face. We pick the songs.</h1>
-        <img alt='' src={Face} style={{display: "inline-block"}}></img> <br />
-        <a href="https://bsr-client.herokuapp.com/">
+        <img alt='' src={Face} style={{display: "inline-block"}}></img> 
+        <br />
+        <a href={`${process.env.REACT_APP_BACKEND_URL}/login`}>
           <button className="green-btn">Login with Spotify</button>
-        </a>   
+        </a>
     </div>
   );
 }
